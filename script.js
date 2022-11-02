bf76e3972c15ea846c03408d5916dc4101943835
dominquelawson <dominque.lawson115@gmail.com>
2022-11-02 10:46:55 -0700

    made buttons function with calling the query for item

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // These constants should hold query parameters to be fed to the map query field
 const shelter = "Shelter";
-const food = "";
-const hospital = "";
-const library = "";
+const mapUrl = `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}`;
+
+mapFrame.setAttribute("src", mapUrl);
 
 debugger;
-mapFrame.setAttribute(
-  "src",
-  `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}`
-);
+function setMapQuery(places) {
+  mapFrame.setAttribute(
+    "src",
+    `https://www.google.com/maps/embed/v1/search?q=${places}&key=${GOOGLE_MAPS_API_KEY}`
+  );
+}
 
 btnContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn")) {
-    alert("call a function that hits tha api and updates the query");
+    const getPlaces = e.target.innerText;
+    setMapQuery(getPlaces);
   }
 });
