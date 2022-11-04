583930254d47dc88854212114f0776daa39a5696
hencito <hencyperez@gmail.com>
2022-11-04 17:57:19 -0400

    refactoring for pages use

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Uncommented for Pages deployment
-// serviceScript.setAttribute(
-//   "src",
-//   `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`
-// );
+/*Uncommented for Pages deployment
+serviceScript.setAttribute(
+  "src",
+  `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`
+);*/
 
 function initMap(itemsToQuery) {
   // debugger;
