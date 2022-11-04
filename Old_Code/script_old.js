c3539f5264ab409149176a45507b83d84f7f933e
hencito <hencyperez@gmail.com>
2022-11-04 17:33:17 -0400

    Refactored All code for final pages site

---
+++ Old_Code/script_old.js
@@ -0,0 +1,62 @@
+// These constants should hold query parameters to be fed to the map query field
+const shelter = "Shelter";
+const mapUrl = `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}`;
+
+mapFrame.setAttribute("src", mapUrl);
+
+debugger;
+
+function setMapQuery(places) {
+  const successCallback = (position) => {
+    //console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const lat_long = String(latitude) + "," + String(longitude);
+
+    mapFrame.setAttribute(
+      "src",
+      `https://www.google.com/maps/embed/v1/search?q=${places}&key=${GOOGLE_MAPS_API_KEY}&center=${lat_long}&zoom=15`
+    );
+  };
+
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    const getPlaces = e.target.innerText;
+    setMapQuery(getPlaces);
+  }
+});
+
+// TODO: see how to optimize this code
+const successCallback = (position) => {
+  console.log(position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const lat_long = String(latitude) + "," + String(longitude);
+
+  mapFrame.setAttribute(
+    "src",
+    `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}&center=${lat_long}&zoom=15`
+  );
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+//quick website improvements
+//detect dark mode
+
+const isDarkMode = () => {
+  window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+};
--- script.js
+++ script.js
@@ -1,65 +1,126 @@
-// These constants should hold query parameters to be fed to the map query field
-const shelter = "Shelter";
-const mapUrl = `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}`;
+serviceScript.setAttribute(
+  "src",
+  `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`
+);
 
-mapFrame.setAttribute("src", mapUrl);
+function initMap(itemsToQuery) {
+  // debugger;
 
-debugger;
-
-function setMapQuery(places) {
-    const successCallback = (position) => {
-        //console.log(position);
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        const lat_long = String(latitude) + "," + String(longitude);
-
-        mapFrame.setAttribute(
-            "src",
-            `https://www.google.com/maps/embed/v1/search?q=${places}&key=${GOOGLE_MAPS_API_KEY}&center=${lat_long}&zoom=15`
-        );
-    };
-
-    const errorCallback = (error) => {
-        console.log(error);
-    };
-
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-}
-
-btnContainer.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn")) {
-        const getPlaces = e.target.innerText;
-        setMapQuery(getPlaces);
-    }
-});
-
-
-// TODO: see how to optimize this code
-const successCallback = (position) => {
-    console.log(position);
+  // Please note that the majority of our map code is within this successCallback function
+  // This is to enable geolocation
+  const successCallback = (position) => {
+    //console.log(position);
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    const lat_long = String(latitude) + "," + String(longitude);
+    const user_Location = { lat: latitude, lng: longitude };
+    // The map, centered at User_Location
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 13,
+      mapId: "d2fddb4cdc51e0d6", // Night Travel custom MapId
+      center: user_Location,
+    });
 
-    mapFrame.setAttribute(
-        "src",
-        `https://www.google.com/maps/embed/v1/search?q=${shelter}&key=${GOOGLE_MAPS_API_KEY}&center=${lat_long}&zoom=15`
+    //TODO: create a way for user to select between two mapIds
+
+    // const darkMap = new google.maps.Map(document.getElementById("map"), {
+    //   zoom: 13,
+    //   mapId: 'd2fddb4cdc51e0d6', // Night Travel custom MapId
+    //   center: user_Location,
+    // });
+
+    // The marker, positioned at user_Location
+    const marker = new google.maps.Marker({
+      position: user_Location,
+      map: map,
+    });
+
+    const service = new google.maps.places.PlacesService(map);
+    let getNextPage;
+    const moreButton = document.getElementById("more");
+    moreButton.onclick = function () {
+      moreButton.disabled = true;
+      if (getNextPage) {
+        getNextPage();
+      }
+    };
+
+    service.textSearch(
+      { location: user_Location, radius: 500, query: itemsToQuery },
+      (results, status, pagination) => {
+        if (status !== "OK" || !results) return;
+        addPlaces(results, map);
+        moreButton.disabled = !pagination || !pagination.hasNextPage;
+        if (pagination && pagination.hasNextPage) {
+          getNextPage = () => {
+            // Note: nextPage will call the same handler function as the initial call
+            pagination.nextPage();
+          };
+        }
+      }
     );
-};
+  };
 
-const errorCallback = (error) => {
+  function addPlaces(places, map) {
+    const placesList = document.getElementById("places");
+    while (placesList.firstChild) {
+      placesList.removeChild(placesList.firstChild);
+    }
+    for (const place of places) {
+      if (place.geometry && place.geometry.location) {
+        const image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+        new google.maps.Marker({
+          map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location,
+        });
+        const li = document.createElement("li");
+        li.textContent = place.name;
+        placesList.appendChild(li);
+        li.addEventListener("click", () => {
+          map.setCenter(place.geometry.location);
+        });
+      }
+    }
+  }
+
+  const errorCallback = (error) => {
     console.log(error);
-};
+  };
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
+initMap("homeless shelter");
 
+// const buttonContainer = document.getElementById("btnContainer");
 
-//quick website improvements
-//detect dark mode
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    const getPlaces = e.target.innerText;
+    resultList.textContent = getPlaces;
 
-const isDarkMode = () => {
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-};
+    //initMap(getPlaces);
+
+    switch (getPlaces) {
+      case " Shelter":
+        initMap("homeless shelter");
+        break;
+      case " Food":
+        initMap("'food pantry' or 'soup kitchen'");
+        break;
+      case " Hospital":
+        initMap("hospital");
+        break;
+      case " Library":
+        initMap("library");
+        break;
+    }
+  }
+});
